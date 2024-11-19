679f728d65b23bcb7563178a294bd3ebbe389fa8
Alessandro Perego <ad7l5zz@mmm.com>
2024-11-18 18:23:34 -0700

    v1.0.12 (#13)
    * 'Updaded_setup.cfg'
    
    * Updaded conftest
    
    ---------
    
    Co-authored-by: Alessandro Perego <aperego@mmm.com>

--- tests/fileio/conftest.py
+++ tests/fileio/conftest.py
@@ -1,14 +1,14 @@
 import os
-import shutil
+from distutils import dir_util
 import pytest
 
 @pytest.fixture()
 def datadir(tmpdir, request):
-    # Locate the directory containing test files
+    # locate the directory containing test files
     filename = request.module.__file__
     test_dir = os.path.join(os.path.dirname(filename), 'test_data')  # Ensure it points to 'test_data' directory
 
     if os.path.isdir(test_dir):
-        shutil.copytree(test_dir, str(tmpdir), dirs_exist_ok=True)  # Copy the entire folder to the tmpdir
+        dir_util.copy_tree(test_dir, str(tmpdir))  # Copy the entire folder to the tmpdir
 
     return tmpdir
